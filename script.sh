c38bc6b5606060378d244390305b607a373afc93
Son Tu <thaisontunguyen@gmail.com>
2022-10-25 15:25:36 -0400

    update

--- script.sh
+++ script.sh
@@ -12,3 +12,11 @@ fi
 
 echo "Copy Contents from ../website/out/ to docs/"
 cp -a ../website/out/. docs/
+
+echo "add docs/ to git"
+git add docs/
+
+read commit_message
+echo "The commit message is" $commit_message
+git commit -m $commit_message
+git push
